e5d574f8d2855488e42d6216891f4cd6f0f163b1
Balazs Kertesz <kertesz.balazs92@gmail.com>
2019-11-06 18:15:30 +0100

    added save feature

--- run.py
+++ run.py
@@ -1,24 +1,29 @@
 from bs4 import BeautifulSoup
 import requests
 
-root_url = "https://en.wikipedia.org/wiki/"
-topic = "Barack_Obama"
-
-get_url = lambda root, topic: root + url 
+get_url = lambda root, topic: root + topic 
 get_html = lambda url: requests.get(url).text
 parse_html = lambda text: BeautifulSoup(text, "html.parser")
 extract_infobox = lambda html: html.find_all("table", {"class": "infobox vcard"}) # TODO: Need better way to specify target
 
-open_file = lambda file_name: open(file_name, "w+")
-write_file = lambda file, content: file.write(content)
-close_file = lambda file: file.close() 
+replace_spaces = lambda text, seperator: text.replace(" ", seperator)
+create_filename = lambda topic: topic + ".md"
 
 def save_infobox(file_name, content):
-    with open(file_name) as file:
-        file.write(content)
+    with open(file_name, "w+") as file:
+        file.write(str(content))
 
 def main():
-    url = get_url(root_url, topic) # TODO: Need to replace spaces with underscore
+    root_url = "https://en.wikipedia.org/wiki/"
+    topic = replace_spaces("Barack Obama", "_")
+
+    url = get_url(root_url, topic)
+    html = parse_html(get_html(url))
+    infobox = extract_infobox(html)
+
+    file_name = create_filename(topic)
+    save_infobox(file_name, infobox)
+
 
 if __name__ == "__main__":
     main()
